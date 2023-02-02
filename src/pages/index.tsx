82d8c7c83b40750f449daacc6b2c5ba72b1c5b8a
mdjorup <michael.djorup@gmail.com>
2023-02-02 15:22:51 -0500

    implemented server side props and fetching user

---
+++ src/pages/index.tsx
@@ -0,0 +1,54 @@
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Auth, withSSRContext } from "aws-amplify";
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
+import { ChatRoom, ListChatRoomsQuery } from "../API";
+import { listChatRooms } from "../graphql/queries";
+
+function Home({ chatRooms: [] }: { chatRooms: ChatRoom[] }) {
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const amplifyUser = await Auth.currentAuthenticatedUser();
+                setUser(amplifyUser);
+            } catch (err) {
+                setUser(undefined);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+    return <div>Hello World!</div>;
+}
+
+export default withAuthenticator(Home);
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const SSR = withSSRContext({ req });
+
+    try {
+        const user = await SSR.Auth.currentAuthenticatedUser();
+        const response = (await SSR.API.graphql({
+            query: listChatRooms,
+            // use authMode: AMAZON_COGNITO_USER_POOLS to make a request on the current user's behalf
+            authMode: "AMAZON_COGNITO_USER_POOLS",
+        })) as {
+            auth: any;
+            data: ListChatRoomsQuery;
+        };
+        return {
+            props: {
+                chatRooms: response.data.listChatRooms?.items,
+            },
+        };
+    } catch {
+        return {
+            props: {
+                chatRooms: [],
+            },
+        };
+    }
+};
